a5c589f61f203fce7558d2f5a37e7709cdb4133c
Guilherme Felipe <glipecoelho2@gmail.com>
2023-10-29 22:44:27 -0300

    Creating the project
    -Created the structure of the project
    -Already coded the "Getters and Setters"

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Studying_JavaBackEnd
  Studying Back-End system using Java.
+
+Technologies already used in this project:
+    Java and Springboot.
---
+++ src/main/java/com/teste/estudo/model/Produto.java
@@ -0,0 +1,66 @@
+package com.teste.estudo.model;
+
+public class Produto {
+    
+    //Integer -> Números menores
+    //Long -> Números maiores
+
+    //#region Atributos
+    private Integer id;
+
+    private String nome;
+
+    private Integer quantidade;
+
+    private Double valor;
+
+    private String observacao;
+
+    //Separando as variaveis com 1 linha de espaço, a geração de getters and setters fica automática 
+    //a partir do caminho: botão direito, ação de origem, generate getters and setters.
+
+    //#endregion
+
+    //#region Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    //#endregion
+
+}
